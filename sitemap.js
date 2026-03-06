2701c6d67ea419127e29d23e51fa76a2c3b26f0a
Caspar Ritchie <caspar.ritchie@lapiscine.pro>
2026-03-06 19:19:47 +0100

    Add pillar pages to sitemap

--- sitemap.js
+++ sitemap.js
@@ -1,54 +1,60 @@
-const fs = require("fs")
+const fs = require("fs");
 
 const cities = [
-"london",
-"manchester",
-"birmingham",
-"leeds",
-"liverpool",
-"bristol",
-"nottingham",
-"sheffield",
-"newcastle",
-"oxford"
-]
+  "london",
+  "manchester",
+  "birmingham",
+  "leeds",
+  "liverpool",
+  "bristol",
+  "nottingham",
+  "sheffield",
+  "newcastle",
+  "oxford"
+];
 
 const projects = [
-"kitchen-renovation",
-"bathroom-renovation",
-"loft-conversion",
-"house-extension",
-"garage-conversion",
-"garden-room",
-"basement-conversion",
-"roof-replacement",
-"window-replacement",
-"rewiring-house"
-]
+  "kitchen-renovation",
+  "bathroom-renovation",
+  "loft-conversion",
+  "house-extension",
+  "garage-conversion",
+  "garden-room",
+  "basement-conversion",
+  "roof-replacement",
+  "window-replacement",
+  "rewiring-house"
+];
 
-let urls = []
+const urls = [];
 
 urls.push(`
 <url>
-<loc>https://www.renovationcostguide.co.uk/</loc>
+  <loc>https://www.renovationcostguide.co.uk/</loc>
 </url>
-`)
+`);
 
-projects.forEach(project => {
-  cities.forEach(city => {
+projects.forEach((project) => {
+  urls.push(`
+<url>
+  <loc>https://www.renovationcostguide.co.uk/cost/${project}</loc>
+</url>
+`);
+
+  cities.forEach((city) => {
     urls.push(`
 <url>
-<loc>https://www.renovationcostguide.co.uk/cost/${project}/${city}</loc>
+  <loc>https://www.renovationcostguide.co.uk/cost/${project}/${city}</loc>
 </url>
-`)
-  })
-})
+`);
+  });
+});
 
 const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${urls.join("")}
-</urlset>`
+</urlset>`;
 
-fs.writeFileSync("frontend/public/sitemap.xml", sitemap)
+fs.writeFileSync("frontend/public/sitemap.xml", sitemap);
 
-console.log("Sitemap generated")
+console.log("Sitemap generated");
