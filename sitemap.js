f0fafe5f1401e5571a3491be55154a95d0756baf
Caspar Ritchie <caspar.ritchie@lapiscine.pro>
2026-03-09 12:11:04 +0100

    Improve sitemap structure and priorities

--- sitemap.js
+++ sitemap.js
@@ -27,8 +27,31 @@ const projects = [
 ];
 
 const staticPages = [
-  "/",
-  "/how-much-does-it-cost-to-renovate-a-house-uk"
+  {
+    path: "/",
+    priority: "1.0",
+    changefreq: "weekly"
+  },
+  {
+    path: "/how-much-does-it-cost-to-renovate-a-house-uk",
+    priority: "0.9",
+    changefreq: "weekly"
+  },
+  {
+    path: "/cost-per-m2-house-renovation-uk",
+    priority: "0.8",
+    changefreq: "weekly"
+  },
+  {
+    path: "/how-much-does-a-loft-conversion-cost-uk",
+    priority: "0.8",
+    changefreq: "weekly"
+  },
+  {
+    path: "/how-much-does-a-house-extension-cost-uk",
+    priority: "0.8",
+    changefreq: "weekly"
+  }
 ];
 
 const urls = [];
@@ -36,7 +59,9 @@ const urls = [];
 staticPages.forEach((page) => {
   urls.push(`
 <url>
-  <loc>https://www.renovationcostguide.co.uk${page}</loc>
+  <loc>https://www.renovationcostguide.co.uk${page.path}</loc>
+  <changefreq>${page.changefreq}</changefreq>
+  <priority>${page.priority}</priority>
 </url>
 `);
 });
@@ -45,6 +70,8 @@ projects.forEach((project) => {
   urls.push(`
 <url>
   <loc>https://www.renovationcostguide.co.uk/cost/${project}</loc>
+  <changefreq>weekly</changefreq>
+  <priority>0.8</priority>
 </url>
 `);
 
@@ -52,6 +79,8 @@ projects.forEach((project) => {
     urls.push(`
 <url>
   <loc>https://www.renovationcostguide.co.uk/cost/${project}/${city}</loc>
+  <changefreq>weekly</changefreq>
+  <priority>0.6</priority>
 </url>
 `);
   });
